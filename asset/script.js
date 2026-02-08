d71789dd527465819111786eb5a1285de9ed3656
lephat <ltmichael52@gmail.com>
2026-02-08 14:14:25 +0700

    fix code javascript

--- asset/script.js
+++ asset/script.js
@@ -1,8 +1,12 @@
+
 var button = $("#changeColorButton");
 function buttonClicked() {
     button.css("color","white")
     button.css("background-color","black")
+    console.log("12345")
 }
 
 
 button.on("click",buttonClicked);
+button.css("color","white")
+
